b06fa575117c50bdbd0cae32e8391f9ac6cc029f
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-07-30 14:29:07 -0300

    Mandando o bloco 4 dia 2

---
+++ trybe/trybe-exercicios/fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript-Array-e-loop-For/exercicios2.js
@@ -0,0 +1,12 @@
+// let numero = 8;
+
+// for (let contador = 0; contador <= 10; contador++) {
+//     console.log(numero * contador)
+// }
+
+
+let aprovadas = ['Maria', 'José', 'Thiago']
+
+for (let contador = 0; contador < aprovadas.length; contador++) {
+    console.log("Bem vindo(a), " + aprovadas[contador] +"!")
+}
