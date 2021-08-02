eeb5fc075110f62481a90cfc436f42b8d597244c
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-08-02 18:22:52 -0300

    Mandando o bloco 4 dia 3

---
+++ trybe/trybe-exercicios/fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia-3-Logica-de-Programação-e-Algoritmo/exercicios.js
@@ -0,0 +1,24 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+
+console.log('Exercicio 1');
+let n = 5;
+let impressao = [];
+for (let index = 1; index <=n; index +=1){
+    for (let index2 = 1; index2 <=n; index2 +=1){
+    impressao += '*';
+    }
+    console.log(impressao);
+    impressao = [null];
+}
+
+
+console.log('Exercicio 2');
+let n = 5;
+let impressao = [];
+for (let index = 1; index <=n; index +=1){
+    for (let index2 = 1; index2 <=n; index2 +=1){
+    impressao += '*';
+    }
+    console.log(impressao);
+    impressao = [null];
+}
